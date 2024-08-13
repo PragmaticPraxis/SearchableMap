3f355f33fcb99b4a59f3d216ce3463a21d20fbe7
Gordon Price <gordon@pragmaticpraxis.com>
2024-08-13 19:04:10 +0300

    Initial Commit

---
+++ SearchableMap/SearchableMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableMapApp.swift
+//  SearchableMap
+//
+//  Created by Gordon Price on 13.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SearchableMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
